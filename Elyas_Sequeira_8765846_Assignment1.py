185f3bb93076fd3fa1a918a9171392ce8163fc83
Elyas <esequeira5846@conestogac.on.ca>
2021-09-30 15:26:58 -0400

    commit on master

---
+++ Elyas_Sequeira_8765846_Assignment1.py
@@ -0,0 +1,30 @@
+#variables
+Shirtprice = float(9.99)
+HST = float(1.13)
+#Commands
+colourChoose = input(str("hello, welcome to Abby's Merchandizing! Ready to order? start by typing which colour of shirt you would like: "))
+#Variable
+cosel = str(colourChoose)
+#Commands
+print("you have chosen a " + colourChoose + " coloured shirt.")
+shirtstyle = input(str("Now select a style of shirt! (Polo/T-shirt) "))
+print("you have selected a " + shirtstyle + " styled shirt.")
+print("please select the amount of shirts you would like: ")
+#Variable
+quant = int(input())
+atax = str(quant * 1.13)
+#Commands
+print("Selected quantity: " + str(quant))
+check= str(input("ready to checkout? 'YES/NO': "))
+if check == (str("YES")):
+    print("your order:")
+    print(str(quant) + " " + colourChoose + " " + shirtstyle + "           " + "    %.2f " % float(str((float(Shirtprice * quant)))))
+    #Variables
+    quantfin = Shirtprice * quant
+    total = float(atax) + quantfin
+    #Command
+    print("tax:                       %.2f "  % float(atax))
+    print("Total ===================  %.2f" % total)
+    print("Thank you for shopping!")
+else:
+    print("please come back soon!")
