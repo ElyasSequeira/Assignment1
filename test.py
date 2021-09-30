1847e2e258560a6a20e660cb3306e0483a33628a
ElyasSequeira <90629095+ElyasSequeira@users.noreply.github.com>
2021-09-30 15:46:13 -0400

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-day = "2021-09-17"
-temp = "22 Degrees Celsius"
-print("Today's day is", day + "!")
-if ("Today's temperature reading is innacurate. "): print("today's temperaure is", temp)
-if temp != "21 Degrees Celsius": print("Today's temperature reading is innacurate, working on it")
-if temp == "21 Degrees Celsius": print("Today's temperature reading is accurate. :)")
-temp = 22
