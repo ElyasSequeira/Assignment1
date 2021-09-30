db4b1c3df71ad02173c42bececa962117459ddc5
ElyasSequeira <90629095+ElyasSequeira@users.noreply.github.com>
2021-09-30 15:45:11 -0400

    Delete Hands on work (flow control).py

--- Hands on work (flow control).py
+++
@@ -1,7 +0,0 @@
-celsius = 0.0
-
-celsius = float(input("enter you temperature in degrees celsius: "))
-degreef = str((celsius * 9/5) +32)
-stringcelsius = str(celsius)
-print ("The value you entered was " + stringcelsius + "degrees Celsius, which is equivallent to " + degreef + " 26degrees Fahrenheit.")\
-
